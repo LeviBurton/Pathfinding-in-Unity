c019de13148226e134bee3321f159d2b10bc88b1
Levi Burton <levi.burton@outlook.com>
2019-01-14 21:23:55 -0600

    maps can load from a texture now.

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class MapData : MonoBehaviour
 {
@@ -10,8 +11,56 @@ public class MapData : MonoBehaviour
     public int height = 5;
 
     public TextAsset textAsset;
+    public string resourcePath = "MapData";
+    public Texture2D textureMap;
 
-    public List<string> GetTextFromFile(TextAsset tAsset)
+    private void Start()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+
+        if (textureMap == null)
+        {
+            textureMap = Resources.Load(resourcePath + "/" + levelName) as Texture2D;
+        }
+
+        if (textAsset == null)
+        {
+            textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
+        }
+    }
+
+    public List<string> GetMapFromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+        if (texture != null)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                string newLine = "";
+                for (int x = 0; x < texture.width; x++)
+                {
+                    if (texture.GetPixel(x, y) == Color.black)
+                    {
+                        newLine += '1';
+                    }
+                    else if (texture.GetPixel(x, y) == Color.white)
+                    {
+                        newLine += '0';
+                    }
+                    else
+                    {
+                        newLine += ' ';
+                    }
+                }
+
+                lines.Add(newLine);
+            }
+        }
+
+        return lines;
+    }
+
+    public List<string> GetMapFromTextFile(TextAsset tAsset)
     {
         List<string> lines = new List<string>();
 
@@ -22,17 +71,19 @@ public class MapData : MonoBehaviour
             lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
             lines.Reverse();
         }
-        else
-        {
-            Debug.LogWarning("MapData GetTextFromFile Error: invalid TextAsset");
-        }
-
+   
         return lines;
     }
 
-    public List<string> GetTextFromFile()
+    public List<string> GetMapFromTextFile()
     {
-        return GetTextFromFile(textAsset);
+        if (textAsset == null)
+        {
+            string levelName = SceneManager.GetActiveScene().name;
+            textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
+        }
+
+        return GetMapFromTextFile(textAsset);
     }
 
     public void SetDimensions(List<string> textLines)
@@ -50,7 +101,16 @@ public class MapData : MonoBehaviour
     public int[,] MakeMap()
     {
         List<string> lines = new List<string>();
-        lines = GetTextFromFile();
+
+        if (textureMap != null)
+        {
+            lines = GetMapFromTexture(textureMap);
+        }
+        else
+        {
+            lines = GetMapFromTextFile(textAsset);
+        }
+
         SetDimensions(lines);
 
         int[,] map = new int[width, height];
